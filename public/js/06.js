e765d0d24cc2997066a3dd2a5e08adb14f32cb1c
junboooi <keio.junboooi@gmail.com>
2015-06-04 12:33:21 +0900

    10番にfalseができなかった。

---
+++ public/js/06.js
@@ -0,0 +1,1 @@
+function fizz(parameter){ if(parameter % 3 == 0){    return "fizz";	}return parameter;}
---
+++ public/js/08.js
@@ -0,0 +1,17 @@
+function fizzbuzz(i){
+   if(i % 3 == 0 && i % 5 == 0){
+   return "fizzbuzz";
+  }
+    else if (i % 3 == 0){
+      return "fizz";
+      }
+    else if (i % 5 == 0){
+      return "buzz";
+      }
+    if(i % 3 == 0 && i % 5 == 0){
+    return "fizzbuzz";
+  }
+  else{
+    return i;
+  }
+}
